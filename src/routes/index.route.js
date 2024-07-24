f384c33d90d2f54b8ab3cfb73664e64854a165d2
Muhammad Adrees <seebiz.bpt0623.dev04@gmail.com>
2024-07-24 23:17:31 +0500

    getApi

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const { Testing, adminLogIn } = require('../controllers/adminLogIn.controller');
 const { blogSubmit, getAllBlogs, getBlogById, blogUpdate, blogDelete } = require('../controllers/blogContent.controller');
 
-route.post('/', Testing);
+route.get('/', Testing);
 route.post('/adminLogIn', adminLogIn);
 route.post('/blogSubmit', blogSubmit);
 route.get('/getAllBlogs', getAllBlogs);
